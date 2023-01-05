449508c6117051260a34b2a3c7e86a48a8b5bb19
mindychxn <mindychxn@gmail.com>
2023-01-05 17:41:23 -0500

    Added keypress to begin game, button animation on click and function to check correctness

--- js/index.js
+++ js/index.js
@@ -1,23 +1,47 @@
 var whichBtn = ["tleft", "tright", "bleft", "bright"];
 var gamePattern = [];
 var userPattern = [];
+var firstPress=false;
+var level=1;
+
+document.addEventListener("keypress", function(){
+    if (!firstPress){
+        $("#level-title").html("Level "+level).css("font-size", "2.5vw");
+        nextSequence();
+        firstPress=true;}
+    });
 
 $(".btn").click(function (event){
     var userChosenBtn = event.target.id;
     userPattern.push(userChosenBtn);
     console.log(userPattern);
+    animatePress(userChosenBtn);
+    checkAns(userPattern.length - 1);
+    
 })
 
 function nextSequence(){
+    $("#level-title").text("Level "+level);
     var randNum = Math.floor(Math.random()*4);
     var chosenBtn = whichBtn[randNum];
     gamePattern.push(chosenBtn);
-    console.log(chosenBtn);
-
-    //$("#" + chosenBtn).fadeOut(100).fadeIn(100);
+    $("#" + chosenBtn).fadeIn(100).fadeOut(100).fadeIn(100);
+    level++;
 }
-nextSequence();
 
-animatePress(currentButton) {
-    $("#"+currentButton).addClass(".pressed");
+function animatePress(currentButton) {
+    $("#"+currentButton).addClass("pressed");
+    setTimeout(function() {$("#"+currentButton).removeClass("pressed")}, 400);
+}
+
+function checkAns(currentLevel){
+    if (userPattern[currentLevel]===gamePattern[currentLevel]){
+        console.log("success");
+        if (userPattern.length===gamePattern.length){
+            setTimeout(nextSequence, 1000);
+        }    
+    }
+    else {
+        console.log("wrong");
+    }
 }
